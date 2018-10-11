60b6b97119e33dc98a76f621a83ef7cba2a17ada
Guilherme <kojofx@gmail.com>
2018-10-11 16:04:50 -0300

    Aula ASP.NET 2.0 Core

---
+++ Aula1307/Aula1307/Models/HomeModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula1307.Models
+{
+    public class HomeModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        
+        public List<HomeModel> listarNomes()
+        {
+            HomeModel item = new HomeModel();
+            List<HomeModel> lista = new List<HomeModel>();
+
+            item.Id = 1;
+            item.Nome = "Mika";
+            lista.Add(item);
+
+            item.Id = 2;
+            item.Nome = "Sarah";
+            lista.Add(item);
+
+            item.Id = 3;
+            item.Nome = "Carol";
+            lista.Add(item);
+
+            return lista;
+        }
+       
+    }
+}
